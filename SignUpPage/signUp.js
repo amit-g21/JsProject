787f932fc9f5a1d12c297eee28a51f28e6a58d17
hilma <amitgonen2468@gmail.com>
2022-11-23 10:55:42 +0200

    upload one

---
+++ SignUpPage/signUp.js
@@ -0,0 +1,32 @@
+let userName = document.querySelector('#userName');
+let password = document.querySelector('#password');
+let checkPassword = document.querySelector('#checkPassword')
+let email = document.querySelector('#email')
+let number = document.querySelector('#phoneNum')
+let btn = document.querySelector('#signUpBtn');
+
+
+
+
+function push(){
+    let userObj =  {
+        username: userName.value, 
+        password: password.value,
+        checkPassword :checkPassword.value,
+        email : email.value,
+        number : number.value
+    }
+    let detalis = [];
+    detalis.push(userObj);
+    localStorage.setItem(userName.value , JSON.stringify(detalis));
+    userName.value = '';
+    password.value = '';
+    checkPassword.value = '';
+    email.value = '';
+    number.value = '';
+    window.location.replace('../homePage/homePage.html')
+
+}
+
+
+btn.addEventListener('click' , push);
---
+++ homePage/homePage.js
@@ -0,0 +1,20 @@
+let userNameLog = document.querySelector('#userNameHome');
+let passwordLog = document.querySelector('#passwordHome');
+let homeBtn = document.querySelector('#logInBtn')
+
+homeBtn.addEventListener('click' , checkDetalis)
+
+
+
+function checkDetalis(){
+    let reco =  JSON.parse(localStorage.getItem(userNameLog.value));
+    console.log(reco)
+    let objPass = reco[0]['password']
+    console.log(objPass)
+    if(reco != null && objPass === passwordLog.value){
+        console.log('good')
+    }else{
+        console.log('kaki')
+    }
+}
+
